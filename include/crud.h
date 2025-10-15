5d4a244cd9844fc31c8290bfbd2ceac901dc880a
vxtinzz <vitin.ti679@gmail.com>
2025-10-14 22:11:24 -0300

    Add files via upload

---
+++ include/crud.h
@@ -0,0 +1,13 @@
+#ifndef CRUD_H
+#define CRUD_H
+
+#include "user.h"
+
+void CREATE();
+void READ(int id);
+void READALL();
+void UPDATE(int id);
+void DELETE(int id);
+void DELETEALL();
+
+#endif
---
+++ include/user.h
@@ -0,0 +1,11 @@
+#ifndef USER_H
+#define USER_H
+
+typedef struct {
+    int id;
+    char name[50];
+    char email[50];
+    int age;
+} User;
+
+#endif
