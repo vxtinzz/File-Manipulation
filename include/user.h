a1cbda64ac5e8242098a35650a4c2df456c9f935
vxtinzz <vitin.ti679@gmail.com>
2026-02-10 23:32:56 -0300

    feat(all-system):add semanticId on all system

--- include/user.h
+++ include/user.h
@@ -3,6 +3,7 @@
 
 typedef struct {
     int id;
+    char semanticId[50];
     char name[50];
     char email[50];
     int age;
--- src/create.c
+++ src/create.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "..\include\user.h"
 #include "..\include\crud.h"
 
@@ -13,8 +14,8 @@ void CREATE()
     arq = fopen("../data/register.txt", "r");
     if (arq != NULL)
     {
-        while (fscanf(arq, "%d;%49[^;];%49[^;];%d\n",
-                      &usuario.id, usuario.name, usuario.email, &usuario.age) != EOF)
+        while (fscanf(arq, "%d;%49[^;];%49[^;];%49[^;];%d\n",
+                      &usuario.id, usuario.semanticId, usuario.name, usuario.email, &usuario.age) != EOF)
         {
             lastId = usuario.id;
         }
@@ -36,6 +37,7 @@ void CREATE()
     printf("Idade: ");
     scanf("%d", &usuario.age);
     usuario.id = lastId + 1;
+    usuario.semanticId = "SUP-YYYY-NUMBER";
 
     fprintf(arq, "%d;%s;%s;%d\n", usuario.id, usuario.name, usuario.email, usuario.age);
     fclose(arq);
